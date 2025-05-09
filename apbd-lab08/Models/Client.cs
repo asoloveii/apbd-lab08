df2db71fd6fab89a46763e2725cc6a8a388ff31b
Arseni Salavei <arsenisoloveii@gmail.com>
2025-05-09 21:26:04 +0200

    feat: created models

---
+++ apbd-lab08/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace apbd_lab08.Models;
+
+public class Client
+{
+    public int IdClient { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Telephone { get; set; } = null!;
+    public string Pesel { get; set; } = null!;
+}
---
+++ apbd-lab08/Models/Trip.cs
@@ -0,0 +1,12 @@
+﻿namespace apbd_lab08.Models;
+
+public class Trip
+{
+    public int IdTrip { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+    public Country Country { get; set; } = null!;
+}
